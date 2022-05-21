1bb4b5b051e17436fc5a741b82554b6acd7703ae
Mateusz <m.stepien1606@gmail.com>
2022-05-21 12:31:21 +0200

    Creating a project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Helllooooo world");
+    }
+    public static void something(){
+        System.out.println("ldfsjnjdfssfdjl");
+    }
+}
